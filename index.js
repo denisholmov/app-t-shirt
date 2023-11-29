238d926acc3dd26326ff79d1c9f7bfd51f757d21
denisholmov <45902177+denisholmov@users.noreply.github.com>
2023-11-29 21:26:09 +0300

    new site

---
+++ index.js
@@ -0,0 +1,22 @@
+function render() {
+  const productsStore = localStorageUtil.getProducts();
+
+  headerPage.render(productsStore.length);
+  productsPage.render();
+}
+
+spinnerPage.render();
+
+let CATALOG = [];
+
+fetch("server/catalog.json")
+  .then((res) => res.json())
+  .then((body) => {
+    CATALOG = body;
+    spinnerPage.handleClear();
+    render();
+  })
+  .catch((error) => {
+    spinnerPage.handleClear();
+    errorPage.render();
+  });
